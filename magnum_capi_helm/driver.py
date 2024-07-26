9358db7cc4eea7d07de004102d1a243c55ade0ff
Jake Yip <jake.yip@ardc.edu.au>
2024-07-26 23:13:13 +1000

    Add availability zone support for instances
    Support availability zones for instances with the following labels:
    - control_plane_availability_zone: A comma separate list of AZs for
      control planes nodes
    - availability_zone: A string with AZ name for worker nodes
    
    Change-Id: Ibee4108323eb6fbad66256e5b154cf66c8938f65

--- magnum_capi_helm/driver.py
+++ magnum_capi_helm/driver.py
@@ -486,6 +486,15 @@ class Driver(driver.Driver):
         except ValueError:
             return default
 
+    def _get_label_array(self, cluster, label, default):
+        cluster_label = self._label(cluster, label, "")
+        if not cluster_label:
+            return default
+        try:
+            return strutils.split_by_commas(cluster_label)
+        except ValueError:
+            return default
+
     def _get_chart_version(self, cluster):
         version = cluster.cluster_template.labels.get(
             "capi_helm_chart_version",
@@ -907,6 +916,29 @@ class Driver(driver.Driver):
                 "ingress": {"enabled": False},
             },
         }
+
+        # Set control plane availability_zone details if they are provided
+        control_plane_availability_zone = self._label(
+            cluster, "control_plane_availability_zone", ""
+        )
+        if control_plane_availability_zone:
+            az_details = {
+                "controlPlane": {
+                    "omitFailureDomain": False,
+                    "failureDomains": [control_plane_availability_zone],
+                },
+            }
+            values = helm.mergeconcat(values, az_details)
+
+        # Set worker availability_zone details if they are provided
+        availability_zone = self._label(cluster, "availability_zone", "")
+        if availability_zone:
+            az_details = {
+                "nodeGroupDefaults": {
+                    "failureDomain": availability_zone,
+                },
+            }
+            values = helm.mergeconcat(values, az_details)
 
         # Add boot disk details, if defined in config file.
         # Helm chart defaults to ephemeral disks, if unset.
